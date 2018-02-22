0b5c52c0828eb5d444f5f0430ed54ee3d28fe839
ubarbaxor <antoine.barbaroux@earthcube.eu>
2018-02-22 23:10:50 +0100

    :wrench: Fixed crash

--- src/store/selectors/index.js
+++ src/store/selectors/index.js
@@ -10,6 +10,10 @@ const view = (state = {}) => Object.keys(state).reduce((acc, symbol) => {
     .toFixed(view.ask
         ? view.ask.split('.')[1].length
         : 1)
+  view.arbitrage = 'disabled'// ( + view.spread || 0)
+    // .toFixed(view.ask
+    //     ? view.ask.split('.')[1].length
+    //     : 1)
   // const arbitrage0 = spread - (view.ask * 0.998 * view.bid * 0.998)
   // const arbitrage1 = spread - (view.ask * 0.9985 * view.bid * 0.9985)
   // const arbitrage2 = (view.ask * 0.9985 + spread) * 0.9985 - view.ask
