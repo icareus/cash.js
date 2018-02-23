a493e2c92943550091e2a1f4fc576222140bdc0a
ubarbaxor <ubarbaxor@gmail.com>
2018-02-23 15:59:56 +0000

    Logs now append

--- src/util/log.js
+++ src/util/log.js
@@ -7,7 +7,7 @@ const {
 
 const logPath = path.join(dir, 'arbitrage.log')
 
-const log = fs.createWriteStream(logPath)
+const log = fs.createWriteStream(logPath, {'flags': 'a'})
 const outputLog = str => log.write(str + '\n')
 
 module.exports = outputLog
